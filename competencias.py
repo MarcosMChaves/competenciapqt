018f069096afddc20436a7e8d6ef511d13e19e7a
MarcosMChaves <marcosdmchaves@gmail.com>
2021-12-06 13:45:36 -0300

    PyQt5 CRUD+L Pessoa no competenciadb

--- competencias.py
+++ competencias.py
@@ -2,12 +2,36 @@ from PyQt5.QtWidgets import QMainWindow, QApplication, QMenu, QMenuBar, QAction,
 from PyQt5 import QtCore, QtWidgets
 from PyQt5 import uic
 
+from pessoa_ie import Ui_Dialog
+
 import MySQLdb as mdb
 import sys
 from database import *
 
 
 class UI(QMainWindow):
+	def janela_inserir_pessoa(self):
+		self.janela = QtWidgets.QDialog()
+		self.ui = Ui_Dialog()
+		self.ui.setupUi(self.janela)
+
+		self.janela.accepted.connect(self.pessoa_inserir)
+
+		self.janela.show()
+
+	def janela_editar_pessoa(self):
+		self.janela = QtWidgets.QDialog()
+		self.ui = Ui_Dialog()
+		self.ui.setupUi(self.janela)
+
+		index = self.tableWidget.currentIndex()
+		pessoa_pk = self.tableWidget.item(index.row(), 0).text()
+		self.ui.nome.setPlainText(self.tableWidget.item(index.row(), 1).text())
+		self.ui.sobrenome.setPlainText(self.tableWidget.item(index.row(), 2).text())
+		self.janela.accepted.connect(lambda: self.pessoa_editar(pessoa_pk))
+
+		self.janela.show()
+
 	def __init__(self):
 		super(UI, self).__init__()
 
@@ -17,11 +41,10 @@ class UI(QMainWindow):
 		self.menubar.addAction(self.exit)
 		self.exit.triggered.connect(self.close)
 
-		self.actionNova.triggered.connect(self.pessoa_inserir)
+		self.actionNova.triggered.connect(self.janela_inserir_pessoa)
+		self.actionEditar.triggered.connect(self.janela_editar_pessoa)
 		self.actionExcluir.triggered.connect(self.pessoa_excluir)
 		
-		#self.tableWidget.itemChanged.connect(self.pessoa_editar)
-
 		self.tableWidget.setColumnWidth(0,0)
 		self.tableWidget.setColumnWidth(1,100)
 		self.tableWidget.setColumnWidth(2,200)
@@ -47,19 +70,20 @@ class UI(QMainWindow):
 			self.tableWidget.setItem(item, 0, QtWidgets.QTableWidgetItem(str(dado[0])))
 			self.tableWidget.setItem(item, 1, QtWidgets.QTableWidgetItem(dado[1]))
 			self.tableWidget.setItem(item, 2, QtWidgets.QTableWidgetItem(dado[2]))
-		#	print(item, dado[0], dado[1])
 			item+=1
 
 		mydb.close()
 
 	def pessoa_inserir(self):
-
 		mydb = self.DBConnection()
 
+		nome = self.ui.nome.toPlainText()
+		sobrenome = self.ui.sobrenome.toPlainText()
+
 		mysql = mydb.cursor()
-		SQL = 'INSERT INTO Pessoa \
+		SQL = f'INSERT INTO Pessoa \
 						(nome_pessoa, sobrenome_pessoa) \
-						VALUES ("Marianna","Chaves")'
+						VALUES ("{nome}","{sobrenome}")'
 		mysql.execute(SQL)
 
 		mydb.commit()
@@ -67,14 +91,9 @@ class UI(QMainWindow):
 
 		self.carregar_tabela()
 
-		QMessageBox.about(self, 'Pessoa', f'Pessoa inserida...')
-
+		QMessageBox.about(self, 'Pessoa', f"Pessoa '{nome} {sobrenome}' inserida...")
 
 	def pessoa_excluir(self):
-		index = self.tableWidget.currentIndex()
-		print(index.row())
-		pessoa_pk = self.tableWidget.item(index.row(), 0).text()
-
 		mydb = self.DBConnection()
 
 		mysql = mydb.cursor()
@@ -83,7 +102,7 @@ class UI(QMainWindow):
 			mysql.execute(SQL)
 			mydb.commit()
 	
-			QMessageBox.about(self, 'Pessoa', f'Pessoa excluída...')
+			QMessageBox.about(self, 'Pessoa', f"Pessoa '{nome} {sobrenome}' excluída...")
 		except:
 			mydb.rollback()
 
@@ -93,12 +112,9 @@ class UI(QMainWindow):
 
 		self.carregar_tabela()
 
-	def pessoa_editar(self):
-		index = self.tableWidget.currentIndex()
-		print('UPD= ',index.row())
-		pessoa_pk = self.tableWidget.item(index.row(), 0)
-		nome = self.tableWidget.item(index.row(), 1)
-		sobrenome = self.tableWidget.item(index.row(), 2)
+	def pessoa_editar(self, pessoa_pk):
+		nome = self.ui.nome.toPlainText()
+		sobrenome = self.ui.sobrenome.toPlainText()
 
 		mydb = self.DBConnection()
 
@@ -111,7 +127,7 @@ class UI(QMainWindow):
 			mysql.execute(SQL)
 			mydb.commit()
 	
-			QMessageBox.about(self, 'Pessoa', f'Pessoa atualizada {SQL}...')
+			QMessageBox.about(self, 'Pessoa', f"Pessoa '{nome} {sobrenome}'atualizada...")
 		except:
 			mydb.rollback()
 
